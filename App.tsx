e5862f591e0f1f7cf6cd216d2ae9a827d0d3f707
Diego <dpadula@epe.santafe.gov.ar>
2025-10-03 11:38:07 -0300

    Mejorado componentes

--- App.tsx
+++ App.tsx
@@ -8,7 +8,7 @@ type MetricProps = {
 
 const Metric = ({ label, value }: MetricProps) => {
   return (
-    <View>
+    <View style={styles.metric}>
       <Text style={styles.label}>{label}</Text>
       <Text style={styles.value}>{value}</Text>
     </View>
@@ -23,21 +23,9 @@ export default function App() {
       <View style={{ flexDirection: 'row', gap: 20 }}>
         <Metric label='Steps' value='12345' />
         <Metric label='Distance' value='0.75km' />
-        {/* <View style={styles.metric}>
-          <Text style={styles.label}>Steps</Text>
-          <Text style={styles.value}>12345</Text>
-        </View>
-        <View style={styles.metric}>
-          <Text style={styles.label}>Distance</Text>
-          <Text style={styles.value}>0.75km</Text>
-        </View> */}
       </View>
 
       <Metric label='Flights Climbed' value='0.75km' />
-      {/* <View>
-        <Text style={styles.label}>Flights Climbed</Text>
-        <Text style={styles.value}>0.75km</Text>
-      </View> */}
       <StatusBar style='auto' />
     </View>
   );
