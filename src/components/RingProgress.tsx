fce3c87ec838d11d8d6c7111e46eb7b7d561ab60
Diego <dpadula@epe.santafe.gov.ar>
2025-10-03 14:09:20 -0300

    agregado de paquete SVG de expo, y prueba dibujo andando

--- src/components/RingProgress.tsx
+++ src/components/RingProgress.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Svg, { Circle, Rect } from 'react-native-svg';
 
 type RingProgressProps = {
   radius?: number;
@@ -18,6 +19,25 @@ const RingProgress = ({ radius = 100 }: RingProgressProps) => {
       }}
     >
       <Text style={{ color: 'white' }}>RingProgress</Text>
+      <Svg height='50%' width='50%' viewBox='0 0 100 100'>
+        <Circle
+          cx='50'
+          cy='50'
+          r='45'
+          stroke='blue'
+          strokeWidth='2.5'
+          fill='green'
+        />
+        <Rect
+          x='15'
+          y='15'
+          width='70'
+          height='70'
+          stroke='red'
+          strokeWidth='2'
+          fill='yellow'
+        />
+      </Svg>
     </View>
   );
 };
