d8cf2a8cd2096c9d89ff00a494d305fbf5f18b5e
Diego <dpadula@epe.santafe.gov.ar>
2025-10-06 14:28:32 -0300

    Agregado de manejo de fechas dinamicamente

--- App.tsx
+++ App.tsx
@@ -1,11 +1,37 @@
+import { AntDesign } from '@expo/vector-icons';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import Metric from './src/components/Metric';
 import RingProgress from './src/components/RingProgress';
 
 export default function App() {
+  const [date, setDate] = useState(new Date());
+  const changeDate = (numDays: number) => {
+    const currentDate = new Date(date); // Create a copy of the current date
+    // Update the date by adding/subtracting the number of days
+    currentDate.setDate(currentDate.getDate() + numDays);
+
+    setDate(currentDate); // Update the state variable
+  };
   return (
     <View style={styles.container}>
+      <View style={styles.datePicker}>
+        <AntDesign
+          onPress={() => changeDate(-1)}
+          name='left'
+          size={20}
+          color='#C3FF53'
+        />
+        <Text style={styles.date}>{date.toDateString()}</Text>
+
+        <AntDesign
+          onPress={() => changeDate(1)}
+          name='right'
+          size={20}
+          color='#C3FF53'
+        />
+      </View>
       <RingProgress radius={100} strokeWidth={40} progress={0.6} />
       <View style={styles.metricContainer}>
         <Metric label='Steps' value='12345' />
@@ -33,5 +59,17 @@ const styles = StyleSheet.create({
     flexWrap: 'wrap',
     gap: 55,
     marginVertical: 50,
+  },
+  datePicker: {
+    alignItems: 'center',
+    padding: 20,
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  date: {
+    color: 'white',
+    fontWeight: '500',
+    fontSize: 20,
+    marginHorizontal: 20,
   },
 });
